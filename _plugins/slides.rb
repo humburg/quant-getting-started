f1e6ace6c883640f96d982227317a41eac20c11c
Dr Peter Humburg <peter.humburg@mq.edu.au>
2019-07-15 15:47:40 +1000

    Generate slides

--- _plugins/slides.rb
+++ _plugins/slides.rb
@@ -15,25 +15,25 @@ module Jekyll
   SLIDE_BEGIN_TAG = "<section.+><div class='content'>"
   SLIDE_CONTENT_END_TAG = "<\/div>"
   SLIDE_END_TAG = "<\/section>"
-  
+
   SLIDE_NOTES_BEGIN_TAG = "<aside class='notes'>"
   SLIDE_NOTES_END_TAG = "</aside>"
-  
+
   SLIDE_VERTICAL_BEGIN_TAG = "<section>"
   SLIDE_VERTICAL_END_TAG = "<\/section>"
-  
+
   SLIDE_FIXUPS = [SLIDE_BEGIN_TAG, SLIDE_CONTENT_END_TAG, SLIDE_END_TAG,
                   SLIDE_VERTICAL_BEGIN_TAG, SLIDE_VERTICAL_END_TAG,
                   SLIDE_NOTES_BEGIN_TAG, SLIDE_NOTES_END_TAG]
-  
+
   class SlideBase < Liquid::Tag
-    
+
     def initialize(tag_name, text, token)
       @tag_name = tag_name
       @text = text
       super
     end
-    
+
     def options
       return @options if @options
       text = @text.to_s.gsub(/\s*:\s*/, ":")
@@ -43,40 +43,40 @@ module Jekyll
     rescue
       @options = {}
     end
-    
+
     def section_end(context)
       out = ""
-      
+
       if context["in_slide_notes"]
         out << "\n\n#{SLIDE_NOTES_END_TAG}\n\n" if context["in_slide_notes"]
       else
         out << "\n\n#{SLIDE_CONTENT_END_TAG}\n\n" if context["in_slide"]
       end
-      
+
       out << "\n\n#{SLIDE_END_TAG}\n\n" if context["in_slide"]
-      
+
       if context["last_vertical_slide"]
         context["last_vertical_slide"] = false
         out << "\n\n#{SLIDE_VERTICAL_END_TAG}\n\n"
       end
-      
+
       context["in_slide_notes"] = false
       context["in_slide"] = false
       out
     end
-    
+
     def section_begin(context)
       context["in_slide"] = true
-      
+
       out = ""
       out << "\n\n#{build_begin_tag}\n\n"
       out
     end
-    
+
     def build_begin_tag
       tag = SLIDE_BEGIN_TAG
       values = "class='slide'"
-      
+
       self.options.each do |k, v|
         values << " data-#{k}='#{v}'"
       end
@@ -84,7 +84,7 @@ module Jekyll
       replaced = tag.gsub(".+", " #{values}")
       return replaced
     end
-    
+
     def render(context)
       if !context.environments.first["page"] || !context.environments.first["page"]["slides"]
         # only if slides
@@ -92,7 +92,7 @@ module Jekyll
       end
       render_tag(context)
     end
-  end  
+  end
   class SlideStart < SlideBase
     def render_tag(context)
       out = ""
@@ -102,37 +102,37 @@ module Jekyll
           out << "\n\n[<h2>View the Presentation</h2>](#{context.environments.first["page"]["url"]}slides)\n<h5>Or view the slide sheet below</h5><hr />\n\n"
         end
       end
-      
+
       out << section_end(context)
       out << before_slide(context)
       out << section_begin(context)
       out
     end
-    
+
     def before_slide(context)
       ""
     end
   end
-  
+
   class SlideEnd < SlideStart
     def render_tag(context)
       section_end(context)
     end
   end
-  
+
   class SlideVerticalFirst < SlideStart
     def before_slide(context)
       out = ""
       if context["in_vertical_slide"]
         out << "\n\n#{SLIDE_VERTICAL_END_TAG}\n\n"
       end
-      
+
       out << "\n\n#{SLIDE_VERTICAL_BEGIN_TAG}\n\n"
       context["in_vertical_slide"] = true
       out
     end
   end
-  
+
   class SlideVerticalLast < SlideStart
     def before_slide(context)
       out = ""
@@ -140,7 +140,7 @@ module Jekyll
       out
     end
   end
-  
+
   class SlideNotes < SlideBase
     def render_tag(context)
       out = ""
@@ -154,14 +154,14 @@ module Jekyll
       out
     end
   end
-  
+
   class Site
     # Render the site to the destination.
     #
     # Returns nothing.
     def render_slides
       payload = site_payload
-      self.posts.each do |post|
+      self.posts.docs.each do |post|
         post.render_slides(self.layouts, payload)
       end
 
@@ -172,7 +172,7 @@ module Jekyll
       # ignore missing layout dir
     end
   end
-  
+
   class Page
     def transform
       super
@@ -181,7 +181,7 @@ module Jekyll
       end
     end
   end
-  
+
   class Post
     def transform
       super
@@ -190,28 +190,27 @@ module Jekyll
       end
     end
   end
-    
+
   class SlidePost < DelegateClass(Post)
     def initialize(page)
       super(page)
-      self.data = page.data.clone
       self.data["layout"] = "slides"
       @source_dir = page.site.source
       @slides_html = "#{page.site.config['slides_folder'] || 'slides'}/index.html"
     end
- 
+
     def slide_destination(page)
       page_path = destination(page)
       page_path.sub('index.html', @slides_html)
     end
-  
+
     def write(dest)
       path = slide_destination(dest)
       FileUtils.mkdir_p(File.dirname(path))
       File.open(path, 'w') do |f|
         f.write(self.output)
       end
-      
+
       # speaker notes
       path = path.sub('index.html', 'notes.html')
       FileUtils.mkdir_p(File.dirname(path))
@@ -219,19 +218,21 @@ module Jekyll
         file = File.join(@source_dir, 'assets', 'slides', 'notes.html')
         f.write(File.read(file))
       end
-      
     end
   end
- 
+
   class Site
     alias _slide_orig_write write
     def write
       _slide_orig_write
- 
-      self.posts.each do |page|
+
+      self.posts.docs.each do |page|
+        puts page.class
+        puts page.class.ancestors
         if page.data["slides"]
           slides = SlidePost.new(page)
-          slides.render(self.layouts, site_payload)
+          # slides.render(self.layouts, site_payload)
+          Jekyll::Renderer.new(self, self, site_payload)
           slides.write(self.dest)
         end
       end
--- _posts/0000-01-01-quant_vs_qual.md
+++ _posts/0000-01-01-quant_vs_qual.md
@@ -4,6 +4,7 @@ sidebar:
   nav: modules
 aside:
   toc: true
+slides: simple
 ---
 
 What research questions are well suited to quantitative analysis and when will
@@ -17,6 +18,8 @@ which questions it can answer. Both Quantitative and qualitative research offer
 powerful methods to deliver new insights. They do, however, excel at different
 things and it is important to use the right tool for the problem at hand.
 
+{% slide %}
+
 > Research Questions
 > {:.title}
 > What are some examples of research questions that are well suited to
@@ -25,12 +28,16 @@ things and it is important to use the right tool for the problem at hand.
 > a question better suited to quantitative analysis on the other.
 {:.exercise}
 
+{% slide %}
+
 > Properties of Quantitative and Qualitative Methods
 > {:.title}
 > Take a look at the research questions that were assigned to the quantitative
 > and qualitative reseach collections. What do questions in each group have
 > in common? What are the differences between the groups?
 {:.discussion}
+
+{% endslide %}
 
 ### Strengths of Qualitative Methods
 
